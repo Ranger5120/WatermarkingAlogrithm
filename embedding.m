61907f806df27c141433779d3b8ed532fd6d7720
Ranger5120 <1269883269@qq.com>
2021-11-16 18:39:26 +0000

    embedding

--- embedding.m
+++ embedding.m
@@ -1,13 +1,17 @@
-I = imread('lenaGray.png');
-
+I = imread('lena.png');
 I = im2double(I);
+I = rgb2gray(I);
 figure(1);imshow(I);
 title('host image');
 
 [ca1,ch1,cv1,cd1]=dwt2(I,'haar');
 [ca2,ch2,cv2,cd2]=dwt2(ca1,'haar');
-figure(2);imshow(ca2);
+figure(2);imshow(ca2);title('ca2 image');
 
 W = imread('watermark.png');
-W=im2double(W);
-imshow(W);
+W = im2double(W);
+W = rgb2gray(W);
+W = 255*W;
+figure(3); imshow(W);
+title('watermark image');
+
