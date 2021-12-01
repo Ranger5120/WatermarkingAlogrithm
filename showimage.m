6790ff824480b6eb60b4ba7e33262fdca37a2cf6
Ranger5120 <1269883269@qq.com>
2021-12-01 02:31:18 +0000

    no commitment

---
+++ showimage.m
@@ -0,0 +1,19 @@
+gh1=imread('gh1.pgm');
+figure(1);imshow(gh1);
+whos gh1;
+
+bt1=imread('bt1.pgm');
+figure(2);imshow(bt1);
+whos bt1;
+
+blk1=imread('blk1.pgm');
+figure(3);imshow(blk1);
+whos blk1;
+
+b21=imread('b21.pgm');
+figure(4);imshow(b21);
+whos b21;
+
+b11=imread('b11.pgm');
+figure(5);imshow(b11);
+whos b11;
